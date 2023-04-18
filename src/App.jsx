a110fedacf9d32e0c36d08bbae92963fab852d6a
Nicolò Cardozo <nic.cardozo@gmail.com>
2023-04-18 16:59:52 +0200

    Project Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
+import List from './List'
 import data from './data'
+import { useState } from 'react'
 
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>
+  const [people, setPeople] = useState(data)
+
+  const clearList = () => {
+    setPeople([])
+  }
+
+  const birthday = people.length === 1 ? 'birthday' : 'birthdays'
+
+  return (
+    <main>
+      <section className="container">
+        <h3>
+          {people.length} {birthday} today:
+        </h3>
+        <List people={people} />
+        <button className="btn btn-block" onClick={clearList}>
+          Clear Birthday List
+        </button>
+      </section>
+    </main>
+  )
 }
 export default App
---
+++ src/Person.jsx
@@ -0,0 +1,12 @@
+const Person = ({ name, age, image }) => {
+  return (
+    <article className="person">
+      <img src={image} className="img" />
+      <div>
+        <h4>{name}</h4>
+        <p>{age}</p>
+      </div>
+    </article>
+  )
+}
+export default Person
